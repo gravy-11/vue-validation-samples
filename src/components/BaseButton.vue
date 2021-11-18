82984c0c01cbdf4d053610ce50c1293b814485df
gravy-11 <gravy667dev@gmail.com>
2021-11-19 01:49:58 +0900

    BaseButtonを修正

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,18 +1,46 @@
 <template>
-  <button class="submit-btn" @click="$emit('click')">
+  <button :class="['submit-btn', sizeClass]" @click="$emit('click')">
     <slot />
   </button>
 </template>
 
 <script lang="ts">
-export default {
+import { computed, defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  props: {
+    size: {
+      type: String as PropType<'md' | 'sm' | 'xs'>,
+      default: 'md',
+    },
+  },
   emits: ['click'],
-};
+  setup(props) {
+    const sizeClass = computed(() => `${props.size}-btn`);
+    console.log('hi');
+
+    return { sizeClass };
+  },
+});
 </script>
 
 <style lang="scss" scoped>
 .submit-btn {
-  width: 120px;
   height: 40px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+.md-btn {
+  width: 120px;
+}
+
+.sm-btn {
+  width: 100px;
+}
+
+.xs-btn {
+  width: 80px;
 }
 </style>
